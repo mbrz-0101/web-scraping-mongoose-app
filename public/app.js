38c47e55687699d4ed989de3b5d222b34e7275dc
Matt Brzowski <5thhand@gmail.com>
2018-06-16 16:47:58 -0700

    add on-click function for new scrape button

--- public/app.js
+++ public/app.js
@@ -7,6 +7,38 @@ $.getJSON("/articles", function(data) {
   }
 });
 
+// Whenever someone clicks a p tag
+$(document).on("click", "#scrape-button", function() {
+  // Empty the notes from the note section
+  $("#articles").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/scrape/"
+  })
+    // With that done, add the note information to the page
+    .then(function(data) {
+      // The title of the article
+      $("#articles").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#articles").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#articles").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#articles").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
 
 // Whenever someone clicks a p tag
 $(document).on("click", "p", function() {
