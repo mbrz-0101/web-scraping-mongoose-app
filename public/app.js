ac87283ae11dc7c567353ad75351f734748efb1e
Matt Brzowski <5thhand@gmail.com>
2018-06-16 17:47:22 -0700

    reformat view

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<h2 data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</h2>");
   }
 });
 
@@ -24,7 +24,7 @@ $(document).on("click", "p", function() {
     .then(function(data) {
       console.log(data);
       // The title of the article
-      $("#notes").append("<p>Title: " + data.title + "</p>");
+      $("#notes").append("<h2>Title: " + data.title + "</h2>");
       // An input to enter a new title
       $("#notes").append("<input id='titleinput' name='title' >");
       // A textarea to add a new note body
