a96f39d923a8f9b602fac5659805177540da75ad
Matt Brzowski <5thhand@gmail.com>
2018-06-16 17:51:23 -0700

    update articles format

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<h2 data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</h2>");
+    $("#articles").append("<a href= " + data[i].link + "><h2 data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</h2></a>");
   }
 });
 
